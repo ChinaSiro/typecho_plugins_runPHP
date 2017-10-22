8ef4a61b45c6514571a0acbeafb97e078a8bbc2f
ChinaSiro <chinasiro@gmail.com>
2017-10-22 13:01:35 +0800

    Create Plugin.php

---
+++ runPHP/Plugin.php
@@ -0,0 +1,64 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+/**
+ * runPHP
+ * 
+ * @package runPHP 
+ * @author ChinaSiro
+ * @version 1.0.0
+ * @link http://hedanku.com
+ */
+class runPHP_Plugin implements Typecho_Plugin_Interface
+{
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     * 
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate(){}    
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     * 
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate(){}
+    
+    /**
+     * 获取插件配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+        /** 分类名称 */
+        $name = new Typecho_Widget_Helper_Form_Element_Text('path', NULL, 'php/', _t('php文件统一存放的目录路径（以/结尾），默认为插件下的php目录'));
+        $form->addInput($name);
+    }
+    
+    /**
+     * 个人用户的配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+    
+    /**
+     * 插件实现方法
+     * 
+     * @access public
+     * @return void
+     */
+    public static function run($runPHP)
+    {
+        include Typecho_Widget::widget('Widget_Options')->plugin('runPHP')->path.$runPHP;
+    }
+}
